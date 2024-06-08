ac52be6b4c04f3349d06424b0eb953ba49a56c62
fefe33 <149985106+fefe33@users.noreply.github.com>
2024-06-08 01:30:01 -0400

    Update server.py
    added proper parsing of command line arguments, output and error are returned as JSON in the response header. length encoded as BE unsigned long long. added more verbose output (--print, -XX flags in tcpdump)

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-import socket, os, json, subprocess, sys
+import socket, os, json, subprocess, sys, struct, argparse
 '''
     the point of this program is to take commands and do specific actions based on those commands
     the client will send commands to the server, 
@@ -32,9 +32,10 @@ import socket, os, json, subprocess, sys
 
 
 class TCPserver:
-    def __init__(self, HOST:str, PORT:int):
+    def __init__(self, HOST:str, PORT:int, debug:bool):
         self.port = PORT
         self.host = HOST
+        self.debug = debug
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #bind to the socket
         self.sock.bind((self.host, self.port))
@@ -51,22 +52,40 @@ class TCPserver:
         #request is of type obj
         if request['cmd'] == 'capture':
             #get the packet limit from the object
-            command = ['sudo', 'tcpdump', '-c', str(request['limit']), '-w', 'remote.pcap']
-              
+            command = ['sudo', 'tcpdump', '-c', str(request['limit'][0]), '-w', 'remote.pcap']
+            try:
+                if request['verbose'] == 'NONE':
+            	    pass
+                elif request['verbose']=='TEXT':
+             	    command.append('--print')
+                elif request['verbose']=='HEX':
+                    command = command+['--print', '-XX']
+            except:
+                pass
             #start the process
+            print('running command: ', repr(' '.join(command)))
             proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             #wait for it to complete and communicate
             proc.wait()
             out = dict()
-             #construct the first response object and start the file transfer
+            #construct the first response object and start the file transfer
             out['put'], out['err'] = proc.communicate()
-             
-            r = [bytes(json.dumps(out['put'].decode('utf-8')), 'utf-8')]
+            if self.debug==True:
+                print('output: ', out['put'], 'error: ', out['err'])
+            #decode each value in the object
+            out['put'] = out['put'].decode('utf-8')
+            out['err'] = out['err'].decode('utf-8')
+            r = bytes(json.dumps(out), 'utf-8')
             
             with open('remote.pcap', 'rb') as pcap:
-                r.append(pcap.read())
-            r = str(len(r[0])).encode('utf-8')+r[0]+r[1]
-            print('payload: ', repr(r))
+                z = pcap.read()
+            #use long long (8 bytes) for header length
+            #get the length (in bytes) of the file (len() function isnt working) 
+            if self.debug==True:
+                print('output length', len(r))
+            r = struct.pack('>Q',len(r))+r+z
+            if self.debug==True:
+                print('payload: ', repr(r))
             #close the current connection and open a new socket not related to self.sock
             print('creating new socket')
             temp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -91,13 +110,12 @@ class TCPserver:
             print('connection attempt from {}'.format(addr))
             #check the connection against the "allow list' table in 
             try: 
-                  print(self.valid_ips)
-                  assert addr[0] in self.valid_ips
-                  print('success. connection accepted.')
+                assert addr[0] in self.valid_ips
+                print('success. connection accepted.')
             except:
-                  print('host not on allow list. closing connection')
-                  csock.close()
-                  continue
+                print('host not on allow list. closing connection')
+                csock.close()
+                continue
         
             #recieve, and process the data 1024 bytes at a time 
             while True:
@@ -106,7 +124,8 @@ class TCPserver:
                     break
                 
                 data = data.decode('utf-8')
-                print(data)
+                
+                print('raw data recieved: ', data)
                 #if its the kill signal
                 if data == '!KILL':
                     sock.close()
@@ -116,6 +135,7 @@ class TCPserver:
                     #deserialize
                     data = json.loads(data)
                     #call request handler function and print the output
+                    
                     print('handler success: ', self.requestHandler(data, csock, addr))
         
         #break the connection and exit the program
@@ -124,20 +144,39 @@ class TCPserver:
         exit(0)
 
 
-#get and validate the args
-args = sys.argv[1:]
-if len(args)!=2:
-	print('incorrect number of arguments. use <host> <port>')
-	exit(-1)
+#get and validate the args with the module
+#only takes 2 -- host and port.
+p = argparse.ArgumentParser()
+p.add_argument('--addr', type=str, nargs=1, help='specifies the host and port the server should listen on [host:port]', required=True)
+p.add_argument('-d','--debug', type=str, help='prints payload length and payload for use in debugging (use \'on\'/\'off\'')
+#parse them
+args = p.parse_args()
+
+#try to get port
 try:
-	#convert the port to integer. 
-	args[1] = int(args[1])
+    address = args.addr[0].split(':')
+    address[1] = int(address[1])
 except:
-	print('port must be of type integer.')
-	exit(-1)
-	
+    print('use <host>:<port> where <port> is an integer. see -h for help.')
+    exit(-1)
+
+#get the debug value as a boolean
+try:
+    if args.debug == 'on':
+        debug = True
+    elif args.debug == 'off':
+        debug = False
+    else:
+        print('invalid arg set for debug (use on/off), setting to default=off')
+        debug=False
+except:
+    print('no --debug arg set. default=off')
+    debug=False
 #if the first arg is *, make hostname empty string
-if args[0] == '*':
-	args[0] = ''
-app = TCPserver(args[0], args[1])
+if address[0] in ['*', '', ' ']:
+	address[0] = ''
+app = TCPserver(address[0], address[1], debug)
 app.service()
+
+
+
